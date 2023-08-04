ed16846f88c9ad4c1cbe0e9445b0253398fa4386
Zheny-mc <68734109+Zheny-mc@users.noreply.github.com>
2023-08-04 12:02:18 +0300

    Добавлен модуль конвертер валют и обработчик ввода.

--- bot_telegram.py
+++ bot_telegram.py
@@ -15,16 +15,20 @@ from currency_converter import CurrencyConverter
 async def on_startup(_):
     print('Бот вышел в онлайн')
 
-convert = CurrencyConverter()
+currency = CurrencyConverter()
 storage = MemoryStorage()
 bot = Bot(token=bot_token())
 dp = Dispatcher(bot, storage=storage)
 
-amount = 0
+amount = 1
+first_curr: str = 'USD'
+second_curr: str = 'EUR'
+
 class FSMAdmin(StatesGroup):
     summa = State()
     f_curr = State()
     s_curr = State()
+
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     await message.answer('Привет, я могу перевести из одной валюты в другую. Например, Eвро / Доллар. Для этого введите команду /convert.')
@@ -55,6 +59,7 @@ async def summa_state(message: types.Message, state: FSMContext):
     except ValueError:
         await message.reply('Неверный формат. Введи сумму!')
         await FSMAdmin.summa.set()
+        await message.delete()
 
 @dp.callback_query_handler(Text(equals='другие'))
 async def other_callback(call_message: types.CallbackQuery):
@@ -64,19 +69,44 @@ async def other_callback(call_message: types.CallbackQuery):
 
 @dp.message_handler(state=FSMAdmin.f_curr)
 async def f_curr_state(message: types.Message, state: FSMContext):
+    global first_curr
+    first_curr = message.text
+    if first_curr not in currency.currencies:
+        await message.reply('Такой валюты не существует!')
+        await FSMAdmin.f_curr.set()
+        await message.delete()
+        return
     await message.reply('Теперь введи вторую валюту')
     await FSMAdmin.s_curr.set()
 
 @dp.message_handler(state=FSMAdmin.s_curr)
 async def s_curr_state(message: types.Message, state: FSMContext):
-    await message.reply('Happy end!')
-    await state.finish()
+    global second_curr
+    second_curr = message.text
+    if second_curr not in currency.currencies:
+        await message.reply('Такой валюты не существует!')
+        await FSMAdmin.f_curr.set()
+        await message.delete()
+        return
+    ##############################
+    res = round(currency.convert(amount, first_curr, second_curr), 1)
+    await bot.send_message(message.chat.id, f'{amount} {first_curr} >==> {res} {second_curr} ')
+    await bot.send_message(message.chat.id, 'Введи сумму:')
+    await FSMAdmin.summa.set()
+    ##############################
+    await message.delete()
 
 
 @dp.callback_query_handler(lambda x: True)
 async def callback(call: types.CallbackQuery):
-    value = call.data.split('/')
-    await bot.send_message(call.message.chat.id, value)
+    curr_f, curr_s = call.data.split('/')
+    res = round(currency.convert(amount, curr_f, curr_s), 1)
+    await bot.send_message(call.message.chat.id, f'{amount} {curr_f} >==> {res} {curr_s} ')
+    await bot.send_message(call.message.chat.id, 'Введи сумму:')
+    await FSMAdmin.summa.set()
     await call.answer()
+    await call.message.delete()
+
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
